2c61706e81e6a88b1afb76f3da773b6f0f6d7675
Erazul Islam <erazul89@gmail.com>
2024-02-26 22:28:38 +0600

    added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
                 <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
                         <FaMoon></FaMoon>
                 </Button>
-                <Link to='/sign-in'>
+                <Link to='/signin'>
                     <Button  gradientDuoTone='purpleToBlue' outline>
                             Sign In
                     </Button>
